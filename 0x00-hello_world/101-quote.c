fd01290f1a95c665eb04618d85e5a12c672e2701
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-13 13:49:27 -0700

    mm

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 01(Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
 	char c [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, c, 100);
+	write(2, c, 59);
 	return (1);
 }
