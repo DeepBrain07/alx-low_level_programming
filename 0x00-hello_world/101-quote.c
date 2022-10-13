e9bfa20ff10ac0dccfecd5d0d3275214419e44ef
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-13 13:46:19 -0700

    mm

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 01(Success)
  */
 int main(void)
 {
-	char c [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char c [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, c, 100);
 	return (1);
 }
