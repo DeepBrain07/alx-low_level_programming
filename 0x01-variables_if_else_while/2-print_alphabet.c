5caaa0766eb2cd90de99fc75ad889da493817891
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-15 14:05:45 -0700

    mm

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,12 +4,13 @@
 /**
  * main - prints alphabet a to z
  *
- * return: Always (0) (Success)
+ * Return: Always (0) (Success)
  */
 int main(void)
 {
 	int i;
-	for (i = 97; i < 122; i++)
+
+	for (i = 97; i < 123; i++)
 	/* this prints alphabet a to z*/
 	{
 		putchar(i);
