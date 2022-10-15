556535ccc170b50fa3f8e0483dfcc236f7ca6086
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-15 14:42:39 -0700

    mm

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all alphabets from a-z except q and e
+ *
+ * Return: Always (0) (Success)
+ */
+int main(void)
+{
+	int i;
+	for  (i = 97; i < 123; i++)
+	/*this prints all aplphabets from a-z*/
+	{
+		if (i == 113 | i == 101)
+			/*prints all alphabets from a-z except q and e*/
+		{
+			continue;
+		}
+		else
+			/*prints all alphabets from a-z except q and e*/
+		{
+			putchar(i);
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
