69eceeb2b1ddb1967045243e1c47cdb02a20117e
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-15 14:16:44 -0700

    mm

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*main - prints alphabets a-z and A-Z
+ *
+ * Return: Always (0) (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		/*this prints alphabet a to z*/
+	{
+		putchar(i);
+	}
+	for (i = 65; i < 91; i++)
+		/*this prints alphabet A to Z*/
+	{
+		putchar(i);
+	}
+	putchar(10);
+
+	return (0);
+}
