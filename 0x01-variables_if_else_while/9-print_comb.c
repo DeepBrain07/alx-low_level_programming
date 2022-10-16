227a29a1d23ec233961fe22af43dc4404f30f705
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-16 14:20:40 -0700

    mm

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,18 +13,15 @@ int main(void)
 	for (i = 48; i < 58; i++)
 		/*prints combination of numbers*/
 	{
-		if (i < 57)
+		putchar(i);
+		if (i != 57)
 			/*prints combination of numbers*/
 		{
-			putchar(i);
 			putchar(44);
+			putchar(32);
 		}
-		else
-		{
-			putchar(i);
-		}
-		putchar(32);
 	}
+	putchar(10);
 
 	return (0);
 }
