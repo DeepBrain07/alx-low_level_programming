0773774a7cd90ba28cb5d2d5d443365b1f115816
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-16 16:08:21 -0700

    mm

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints combination of two digits
+ *
+ * Return: Always (0) (Success)
+ */
+int main(void)
+{
+	int a = 48;
+	int b = 48;
+
+	while (a < 58)
+	{
+		while (b < 58)
+		{
+			if ((a == b) & (b > a))
+			{
+				continue;
+			}
+			else
+			{
+				putchar(a);
+				putchar(b);
+				if ((a == 57) & (b == 56))
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+
+	return (0);
+}
