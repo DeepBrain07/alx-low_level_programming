5915e74fb488438cb602394322b7111bfc623b22
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-18 16:12:37 -0700

    mm

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints out _putchar
+ *
+ * Return: Always (0) (Success)
+ */
+int main(void)
+{
+	_putchar(_putchar);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+char _putchar(char input[])
+{
+	return (printf("%s\n", input));
+}
