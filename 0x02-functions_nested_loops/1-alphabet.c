eb16f08f386ac8a9cd1cad0f0bc6c4ce045307e0
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-18 23:13:40 -0700

    mm

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
-/**main - prints out alphabets
- * Return: Always (0)
- */
 void print_alphabet(void)
 /*this function prints out alphabets*/
 {
+/*this function prints out alphabets*/
 	int x;
 
 	for (x = 97; x < 123; x++)
