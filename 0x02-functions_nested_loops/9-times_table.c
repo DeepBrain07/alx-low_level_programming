588a1188a8f2e0b6c546d4839f6045d60c47c56b
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-19 14:15:36 -0700

    mm

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * times_table - prints times table 9
+ *
+ * Return: returns nothing
+ */
+void times_table(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			int r = x * y;
+			
+			if (r < 10)
+			{
+				_putchar(r + 48);
+				if (r = 9)
+				{
+					break;
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				int a = r / 10;
+				int b = r % 10;
+				
+				_putchar(a + 48);		
+				_putchar(b + 48);
+				if (y = 9)
+				{
+					break;
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
