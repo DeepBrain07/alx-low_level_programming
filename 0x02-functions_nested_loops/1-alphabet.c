a1108d001a74b93095808a187e8819b061231d54
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-18 22:53:16 -0700

    m

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**main - prints out alphabets
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 97; x < 123; x++)
+	/*prints out alphabets*/
+	{
+		_putchar(x);
+	}
+	_putchar(10);
+
+	return (0);
+}	
