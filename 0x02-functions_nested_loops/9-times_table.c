327372fd2df3ea52c0b3dd8783aa987d59cc9465
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-19 14:47:30 -0700

    mm

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,32 +11,26 @@ void times_table(void)
 
 	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y < 10; y++)
+		_putchar(48);
+		_putchar(',');
+		for (y = 1; y < 10; y++)
 		{
 			int r = x * y;
 			
-			if (r < 10)
+			if ( r < 10)
 			{
+				_putchar(' ');
+				_putchar(' ');
 				_putchar(r + 48);
-				if (y < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
 			}
 			else
 			{
 				int a = r / 10;
 				int b = r % 10;
-				
+
+				_putchar(' ');
 				_putchar(a + 48);
 				_putchar(b + 48);
-				if (y < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
 			}
 		}
 		_putchar('\n');
