7fd19e1b6c43a35a42db3efad5c89c9771c35628
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-19 15:28:09 -0700

    mm

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints natural numbers from n to 98
+ *
+ * @n: must be an integer
+ *Return: returns (0)
+ */
+ void print_to_98(int n)
+{
+	int x;
+
+	for (x = n; x < 99; x++)
+	{
+		if (x < 0)
+		{
+			_putchar(-x + 48);
+		}
+		else
+		{
+			_putchar(x + 48);
+		}
+	}
+	_putchar('\n');
+
+	return (0);
+}
