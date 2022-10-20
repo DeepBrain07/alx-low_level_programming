094d20c9779ebb050a9eaaac0ce95668f1d67c2a
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-20 15:43:56 -0700

    mm

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,10 @@
 #ifndef main_H
 #define main_H
 
-void print_numbers(void);
-int mul(int a, int b);
-int _isdigit(int c);
-int _isupper(int c);
 int _putchar(int c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
 
 #endif
