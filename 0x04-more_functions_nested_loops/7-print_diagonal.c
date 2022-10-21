801de5c97d5abfadf1c2f744de1ea03540c1c880
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-21 14:33:45 -0700

    mm

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,24 +7,24 @@
 */
 void print_diagonal(int n)
 {
-    int i;
-    int j;
+	int i;
+    	int j;
 
-    for (i = 0; i < n; i++)
-    {
-        if (n > 0)
-        {
-            for (j = 0; j < i; j++)
-            {
-                _putchar(' ');
-            }
-            _putchar('\\');
-            _putchar('\n');
-        }
-        else
-        {
-            break;
-        }
-    }
-    _putchar('\n');
+    	for (i = 0; i < n; i++)
+    	{
+        	if (n > 0)
+        	{
+            	for (j = 0; j < i; j++)
+            	{
+                	_putchar(' ');
+            	}
+            	_putchar('\\');
+            	_putchar('\n');
+        	}
+        	else
+        	{
+            	break;
+        	}
+    	}
+    	_putchar('\n');
 }
