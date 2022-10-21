b7c4eedcedaffcd68edc57619fd6ef08af76f9e1
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-21 14:56:40 -0700

    m

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,15 +10,15 @@ void print_line(int n)
 	int i;
 
 	if (n > 0)
-    	{
-        	for (i = 0; i < n; i++)
-        	{
-            		_putchar('_');
-        	}
-        	_putchar('\n');
-    	}
-    	else
-    	{
-        	_putchar('\n');
-    	}
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
