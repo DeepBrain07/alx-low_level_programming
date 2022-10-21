8362ce4974bdebe408a6fd08e6ecd74ca6543004
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-21 10:47:00 -0700

    mm

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*more_numbers - prints numbers from 0 to 9 ten times
+*
+*Return: nothing
+*/
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j/10) + 48);
+			}
+			_putchar(j % 10);
+		}
+		_putchar('\n');
+	}
+}
