d6dd36d1c4c56527a5534369f560bd1d5995ef88
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-24 13:53:53 -0700

    mm

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+*puts_half - prints half of the string
+*@str: the string
+*Return: nothing
+*/
+void puts_half(char *str)
+{
+	int i, x;
+
+	x = strlen(str);
+	for (i = x / 2; i < x; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
