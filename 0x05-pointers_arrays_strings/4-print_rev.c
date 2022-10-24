5d0a78df730b7be90d0bd8469eb54f37db0fcdf3
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-24 11:12:24 -0700

    mm

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+*print_rev - prints a string in reverse
+*@s: a string
+*@Return: nothing
+*/
+void print_rev(char *s)
+{
+	strrev(s);
+	_puts(strrev(s));
+}
