81a82ae6f39d6c06085ceaecb2e5c23f339eee74
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-24 13:46:07 -0700

    mm

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+
+/**
+*rev_string - reverses a string
+*@s: a string
+*Return: nothing
+*/
+void rev_string(char *s)
+{
+
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+*puts2 - prints every other characters of a string
+*@str: a string
+*Result: nothing
+*/
+void puts2(char *str)
+{
+	int x, y;
+
+	y = strlen(s);
+	for (x = 0; x < y; x++)
+	{
+		if (x % 2 == 0)
+		{
+			_putchar(str[x]);
+		}
+	}
+	_putchar('\n');
+}
