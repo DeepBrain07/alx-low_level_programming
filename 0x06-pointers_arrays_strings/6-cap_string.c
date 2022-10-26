9936184ee56e4620d2dee3d3783871d7b1e29c51
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-26 15:54:59 -0700

    mm

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,14 +16,15 @@ char *cap_string(char *str)
 	}
 	for (i = 1; str[i]; i++)
 	{
-		if (str[i] == ',')
+		j = str[i];
+		if (j == 32 || j == 9 || j == 10 || j == 44 || j == 59 || j == 46 || j == 33 || j == 34 || j == 63 || j == 40 || j == 41 || j == 123 || j == 125)
 		{
-			j = str[i + 2];
-			if (j > 96 && j < 123)
-			{
-				j = j - 32;
-				str[i + 2] = j;
-			}
+			++i;
+		}
+		if (j > 96 && j < 123)
+		{
+			j = j - 32;
+			str[i] = j;
 		}
 	}
 	return (str);
