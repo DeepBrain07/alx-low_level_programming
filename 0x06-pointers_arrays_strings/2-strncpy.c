7b050dd15e9399e4ee2bee0690a43abd1bb2db1d
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-26 12:21:01 -0700

    m

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*_strncpy - copies a string
+*@dest: destination
+*@src: source
+*@n: number of byt(s)
+*Return: Dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+		if (src[i] == '\0')
+		{
+			break;
+		}
+	}
+	return (dest);
+}
