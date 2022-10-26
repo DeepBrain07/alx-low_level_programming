b8224146980206746a8e15bbc1c7433add737986
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-26 10:55:30 -0700

    m

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,7 +13,7 @@ char *_strcat(char *dest, char *src)
 	counter = 0;
 	for (i = 0; dest[i] != '\0'; i++)
 	{
-		counter += 1;
+		counter = counter + 1;
 	}
 	for (j = 0; src[j]; j++)
 	{
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,7 +14,7 @@ char *_strncat(char *dest, char *src, int n)
 	counter = 0;
 	for (i = 0; dest[i] != '\0'; i++)
 	{
-		counter = counter 1;
+		counter = counter + 1;
 	}
 	for (j = 0; j <= n; j++)
 	{
