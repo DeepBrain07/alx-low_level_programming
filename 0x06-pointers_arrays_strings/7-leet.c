0fae6224ab7e2cf22af388c90d1e130c49b5a891
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-26 16:52:47 -0700

    mm

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,16 +7,16 @@
 char *leet(char *str)
 {
 	int i, j;
-	int low_letters = {97, 101, 111, 116, 108};
-	int up_letters = {65, 69, 79, 84, 76};
-	int num = {4, 3, 0, 7, 1};
+	int low_letters[] = {97, 101, 111, 116, 108};
+	int up_letters[] = {65, 69, 79, 84, 76};
+	int num[] = {4, 3, 0, 7, 1};
 
 	for (i = 0; str[i]; i++)
 	{
 		j = str[i];
 		if (j == low_letters[i] && j == up_letters[i])
 		{
-			j = num + 48;
+			j = num[i] + 48;
 		}
 	}
 	return (str);
