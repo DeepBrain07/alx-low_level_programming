dc509b4c1c10d838ed2385f747999580df1d6c55
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-26 12:27:23 -0700

    mm

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,11 +12,19 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		dest[i] = src[i];
 		if (src[i] == '\0')
 		{
 			break;
 		}
+		else
+		{
+			dest[i] = src[i];
+		}
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = '\0';
 	}
 	return (dest);
 }
