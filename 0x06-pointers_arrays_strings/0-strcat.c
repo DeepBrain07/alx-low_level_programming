8e25370285b07530233137f13ac3cf6efe2f79cc
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-26 10:23:41 -0700

    mm

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*_strcat - concatenates two strings
+*@dest: a string
+*@src: a string
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i, j, counter;
+	
+	counter = 0;
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		counter += 1;
+	}
+	for (j = 0; src[j]; j++)
+	{
+		dest[counter + j] = src[j];
+	}
+	
+	return (dest);
+}
