07966cd740376d1ad7e6784c3e424187956fb7af
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-26 13:37:23 -0700

    mm

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,13 +9,15 @@ int _strcmp(char *s1, char *s2)
 {
 	int j, i;
 
+	j = 0;
+	i = 0;
 	while (j == 0)
 	{
-		if (s1[j] == '\0' && s2[j] == '\0')
+		if (s1[i] == '\0' && s2[i] == '\0')
 		{
 			break;
 		}
-		i = s1[j] - s2[j];
+		j = s1[i] - s2[i];
 	}
-	return (i);
+	return (j);
 }
