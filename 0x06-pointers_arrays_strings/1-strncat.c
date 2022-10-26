240e3d90d461f8f55287148642fae868f798fd1b
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-26 10:52:31 -0700

    m

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*_strncat - concatenates two strings
+*@dest: destination
+*@src: source
+*@n: number of byte(s)
+*Result: dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, counter;
+
+	counter = 0;
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		counter = counter 1;
+	}
+	for (j = 0; j <= n; j++)
+	{
+		dest[counter + j] = src[j];
+		if (src[j] == '\0')
+		{
+			break;
+		}
+	}
+	return (dest);
+}
