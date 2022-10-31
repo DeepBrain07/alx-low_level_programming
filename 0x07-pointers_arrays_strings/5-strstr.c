3adad3de7c376e7a8f4c27427bac7d0da209ed2c
Timilehin Olusa <olusatimmie07@gmail.com>
2022-10-31 13:28:39 -0700

    mm

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+*_strstr - locates a substring
+*@haystack: a string
+*@needle: a string
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, k, m;
+
+	for (i = 0; needle[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; haystack[j] != '\0'; j++)
+	{
+		if (haystack[j] == needle[0])
+		{
+			for (k = 0; needle[k]; k++)
+			{
+				if (haystack[j] == needle[k])
+				{
+					j++;
+				}
+			}
+			for ((m = j - i); haystack[m]; m++)
+			{
+				return (haystack + m);
+			}
+		}
+	}
+	return ('\0');
+}
