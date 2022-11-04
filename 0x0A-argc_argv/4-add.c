42b087f0d65c4cd408f8a032d7a431302a4e5daa
Timilehin Olusa <olusatimmie07@gmail.com>
2022-11-04 15:35:18 -0700

    mm

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - adds numbers
+*@argc: argument count
+*@argv: argument vector
+*Return: an integer
+*/
+int main(int argc, char *argv[])
+{
+	int i, j, k, l;
+
+	l = 0;
+	if (argc < 2)
+		printf("%d\n", 0);
+	for (i = 1; i < argc; i++)
+	{
+		j = atoi(argv[i]);
+		for (k = 0; k < 10; k++)
+		{
+			if (j != k)
+			{
+				printf("Error\n");
+				return (1);
+				i = argc;
+				break;
+			}
+			else
+			{
+				l = l + j;
+				break;
+			}
+		}
+	}
+	printf("%d\n", l);
+	return (0);
+}
