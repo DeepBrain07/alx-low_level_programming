989f93c79f77849a039507894c93580738740543
Timilehin Olusa <olusatimmie07@gmail.com>
2022-11-04 14:31:44 -0700

    mm

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,8 +5,8 @@
 *@argv: argument vector
 *Return: '0' for success
 */
-int main(int argc, char *argv[] __attribut__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
