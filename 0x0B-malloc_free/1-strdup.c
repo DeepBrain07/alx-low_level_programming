9985f617e5dd090370a3d63750745946562fba06
Timilehin Olusa <olusatimmie07@gmail.com>
2022-11-08 06:58:33 -0800

    mm

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+*_strdup - duplicates a string
+*@str: a string
+*Return: a pointer to the duplicated string
+*/
+char *_strdup(char *str)
+{
+	int i, counter;
+	char *myArray;
+
+	counter = 0;
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		counter += 1;
+	myArray = malloc(sizeof(char) * counter);
+	for(i = 0; str[i]; i++)
+		*(myArray + i) = str[i];
+	return (myArray);
+}
