16745479f8e729663e803a0ac0c097c1f9486149
Timilehin Olusa <olusatimmie07@gmail.com>
2022-11-11 07:45:13 -0800

    mm

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,9 +15,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 
 	if (s1 == NULL)
-		s2 = "";
-	if (s2 == NULL)
 		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		count += 1;
 	for (i = 0; s2[i] != '\0'; i++)
