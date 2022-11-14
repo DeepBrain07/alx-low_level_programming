113e1ade7b4b50ceb98820cfcf36704ba5ca27cb
Timilehin Olusa <olusatimmie07@gmail.com>
2022-11-14 13:02:07 -0800

    mm

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog {
-	char name[100];
+/**
+*struct dog - struct that stores information about a dog
+*@name: name of the dog
+*@age: the Dog's age
+*@owner: owner of the dog
+*
+*Description: stores information about a dog
+*/
+struct dog
+{
+	char *name;
 	float age;
-	char owner[100];
+	char *owner;
 };
 
 #endif
