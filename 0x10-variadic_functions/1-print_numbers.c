d668b1764e06a84eaa37a7d686eb2e87d7a63bdd
Timilehin Olusa <olusatimmie07@gmail.com>
2022-11-17 06:01:54 -0800

    mm

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,36 +9,18 @@
 */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	void print(long int x)
-	{
-		if (x < 0)
-		{
-			_putchar('-');
-			x = -x;
-		}
-		if (x / 10)
-			print(x / 10);
-		_putchar((x % 10) + '0');
-	}
-	unsigned int i, j, k, count;
-	if (separator)
-		for (i = 0; separator[i]; i++)
-			count += 1;
+	unsigned int i;
 	va_list list;
 
 	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
-		j = va_arg(list, int);
-		if (j >= 10)
-			print(j);
-		else
-			_putchar(j + 48);
+		printf("%d", va_arg(list, int));
 		if (i != (n - 1) && separator)
-			for (k = 0; k <= (count); k++)
-				_putchar(separator[k]);
+			printf("%s", separator);
 	}
+	printf("\n");
 
 	va_end(list);
 }
