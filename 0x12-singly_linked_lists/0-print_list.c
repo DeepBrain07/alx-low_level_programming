575a88b78faf391d04235b45a0b68f5d686bd173
Timilehin Olusa <olusatimmie07@gmail.com>
2022-11-24 13:41:32 -0800

    mm

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,8 @@
 #include "lists.h"
-
 /**
 *print_list - prints all the elements of a list_t list
 *@h: temporal pointer
+*
 *Return: number of nodes
 */
 
@@ -19,6 +19,5 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		count += 1;
 	}
-
 	return(count);
 }
