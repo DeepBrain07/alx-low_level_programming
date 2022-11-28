2824847ee22f13754283444c6c3c8156c190f45c
Timilehin Olusa <olusatimmie07@gmail.com>
2022-11-28 10:26:06 -0800

    mm

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+*free_listint - frees a list
+*@head: head of the list
+*Return: nothing
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *assign;
+	while ((assign = head) != NULL)
+	{
+		free(assign);
+		head = head->next;
+	}
+}
