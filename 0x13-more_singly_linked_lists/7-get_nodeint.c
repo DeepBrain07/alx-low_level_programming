58561914540060f2acef18baaf94c0b49c693e01
Timilehin Olusa <olusatimmie07@gmail.com>
2022-11-29 09:22:48 -0800

    Task 7 done

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,14 +9,9 @@
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int count = 0;
+	unsigned int i;
 
-	if (head && index)
-		while (head != NULL)
-		{
-			if (count == index)
-				return (head);
-			count++;
-		}
-	return (NULL);
+	for (i = 0; i < index && head != NULL; i++)
+		head = head->next;
+	return (head);
 }
