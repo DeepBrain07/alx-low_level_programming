d3ae53ec608b168f5a75cbd1ed997b70a63a593d
Timilehin Olusa <olusatimmie07@gmail.com>
2022-11-29 19:03:01 -0800

    Task 10 done

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	assign = *head;
 	if (index != 0)
-		for (i = 0; i <= index - 1 && assign != NULL; i++)
+		for (i = 0; i < index - 1 && assign != NULL; i++)
 			assign = assign->next;
 	if (assign == NULL || (assign->next == NULL && index != 0))
 		return (1);
