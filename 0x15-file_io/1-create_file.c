531d5b80f18b00b89534a7d92b1f7144f984a0c4
Timilehin Olusa <olusatimmie07@gmail.com>
2022-12-05 14:37:48 -0800

    Task 1 done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,18 +8,20 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i;
+	int fd, i, wc;
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDWR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
 	if (!text_content)
-		write(fd, "", 0);
-	else
-	{
-		for (i = 0; text_content[i]; i++)
-			;
-		write(fd, text_content, i);
-	}
+		text_content = "";
+	for (i = 0; text_content[i]; i++)
+		;
+	wc = write(fd, text_content, i);
+	if (wc < 0)
+		return (-1);
+	close(fd);
 	return (1);
 }
