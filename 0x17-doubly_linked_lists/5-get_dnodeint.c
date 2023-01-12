89f5a3feaf0ba402afb61a57b15dca583eafe776
Timilehin Olusa <olusatimmie07@gmail.com>
2023-01-12 15:20:27 -0800

    Task 5 debugged

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,6 +11,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	if (!head || !index)
 		return (NULL);
+	while (head->prev != NULL)
+		head = head->prev;
 	while (head)
 	{
 		if (count == index)
