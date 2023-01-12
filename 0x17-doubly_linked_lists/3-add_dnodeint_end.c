b82880984ee8fc920545f34f4ccd31f5f9e9f3bf
Timilehin Olusa <olusatimmie07@gmail.com>
2023-01-12 14:48:30 -0800

    Task 3 debugged

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -16,7 +16,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 	h = *head;
-	if (h->next != NULL)
+	while (h->next != NULL)
 		h = h->next;
 	new->n = n;
 	new->next = NULL;
