05a9809832f20a5c54b2b7f22a240cf88ec2d614
Timilehin Olusa <olusatimmie07@gmail.com>
2023-01-13 13:20:56 -0800

    Task 7 debugged

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *hd;
 	unsigned int count = 1;
 
-	new = NULL
+	new = NULL;
 	if (!h || !idx || !n)
 		return (NULL);
 	if (idx == 0)
